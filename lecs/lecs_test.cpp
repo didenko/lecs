a338a934522a1942f4a5ad1d3d88a5e302d94da3
Vlad Didenko <business@didenko.com>
2016-11-28 21:32:42 -0600

    Advancing the lecs::Context tests - not done yet

--- lecs/lecs_test.cpp
+++ lecs/lecs_test.cpp
@@ -10,6 +10,8 @@
 
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <thread>
 
 #include <gtest/gtest.h>
 #include "context.hpp"
@@ -17,32 +19,120 @@
 namespace tests {
 namespace lecs {
 
+using namespace std::placeholders;
+
+template<class outer_type>
+const typename outer_type::value_type flatten(const outer_type &outer)
+{
+  typename outer_type::value_type flat;
+  for (auto &inner: outer)
+  {
+    flat.insert(flat.end(), inner.begin(), inner.end());
+  }
+  return flat;
+};
+
+using message_vec = std::vector<std::vector<std::string>>;
+
+const message_vec messages{
+  {""},
+  {"", ""},
+  {"abc", "1234567890", "ABCDEFGHIJ"},
+  {"", "XYZ", ""}
+};
+
+const message_vec::value_type messages_flat{flatten(messages)};
+
+class TestContext: public ::lecs::Context
+{
+public:
+  TestContext() : ::lecs::Context(
+    std::bind(&TestContext::onconn, this, _1),
+    std::bind(&TestContext::ondisc, this, _1),
+    std::bind(&TestContext::intake, this, _1, _2)
+  )
+  {}
+
+  void onconn(asion::connection_ptr c)
+  {
+    conn = c;
+    cursor = messages_flat.begin();
+    message_idx = 0;
+  }
+
+  void ondisc(asion::connection_ptr c)
+  {
+    ASSERT_EQ(conn, c) << "Connection mismatch";
+  }
+
+  void intake(asion::connection_ptr c, ::lecs::Message &&received)
+  {
+    ASSERT_EQ(conn, c) << "Connection mismatch";
+    ASSERT_NE(cursor, messages_flat.end()) << "Received message is unexpected: " << received;
+    ASSERT_EQ(*cursor, received) << "Received message differs from the expected, idx " << message_idx;
+    ++cursor;
+    ++message_idx;
+  }
+
+  void finished()
+  {
+    ASSERT_EQ(cursor, messages_flat.end()) << "Did not receive all expected messages";
+  }
+
+protected:
+  message_vec::value_type::const_iterator cursor{messages_flat.begin()};
+  uint message_idx{0};
+
+  asion::connection_ptr conn;
+};
+
 struct Nodes: public ::testing::Test
 {
-  ::lecs::Context handlers{
-    [](::asion::connection_ptr conn) {
-      std::cerr << "Connection from: " << std::string(conn->endpoint_remote()) << std::endl;
-    },
-    [](::asion::connection_ptr conn) {
-      std::cerr << "Disconnected from: " << std::string(conn->endpoint_remote()) << std::endl;
-    },
-    [this](::asion::connection_ptr conn, ::lecs::Message m) {
-      handlers.write(conn, m + "\n");
-      std::cout << std::string(conn->endpoint_remote()) << " :> " << m << std::endl;
-    }
-  };
-
-  asion::context_ptr context = handlers.node_context();
+  TestContext context;
 
   asion::Node
-    server{context},
-    client{context},
-    cliser{context};
+    server{context.node_context(), "0.0.0.0", "50001"};
+
+  std::thread server_thread;
+
+protected:
+  virtual void SetUp()
+  {
+    server_thread = std::thread(
+      std::bind(&asion::Node::run, &server)
+    );
+//    client.run();
+//    cliser.run();
+  }
+
+  virtual void TearDown()
+  {
+    server.shutdown();
+    server_thread.join();
+//    client.shutdown();
+//    cliser.shutdown();
+  }
+
 };
 
 TEST_F(Nodes, connect)
 {
+  ASSERT_TRUE(true);
 }
+
+TEST(Flatten, strings)
+{
+  std::vector<std::vector<std::string>> in = {
+    {""},
+    {"", ""},
+    {"abc", "1234567890", "ABCDEFGHIJ"},
+    {"", "XYZ", ""}
+  };
+
+  std::vector<std::string> out = {"", "", "", "abc", "1234567890", "ABCDEFGHIJ", "", "XYZ", ""};
+
+  ASSERT_EQ(out, flatten(in));
+};
 
 }
 }
